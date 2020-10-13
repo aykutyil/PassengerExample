2a02fa0649492505c0d4fbfb868ce1a6d935faa3
passengerdata <67289785+aykutyil@users.noreply.github.com>
2020-10-13 18:31:19 +0300

    progress bar test

--- app/src/main/java/com/example/passengerpagination/ui/PassengerFragment.kt
+++ app/src/main/java/com/example/passengerpagination/ui/PassengerFragment.kt
@@ -1,22 +1,18 @@
 package com.example.passengerpagination.ui
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.os.HandlerCompat.postDelayed
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.passengerpagination.adapter.PassengerAdapter
 import com.example.passengerpagination.databinding.FragmentPassengerBinding
 import com.example.passengerpagination.viewmodel.PassengerFragmentViewModel
-import kotlinx.android.synthetic.main.fragment_passenger.*
 
 class PassengerFragment : Fragment() {
 
@@ -41,8 +37,8 @@ class PassengerFragment : Fragment() {
 
         viewModel = ViewModelProviders.of(this).get(PassengerFragmentViewModel::class.java)
 
-        passenger_recyclerview.layoutManager = LinearLayoutManager(context)
-        passenger_recyclerview.adapter = adapter
+        binding.passengerRecyclerview.layoutManager = LinearLayoutManager(context)
+        binding.passengerRecyclerview.adapter = adapter
 
         begin()
 
@@ -58,13 +54,13 @@ class PassengerFragment : Fragment() {
     private fun networkCheck() {
         viewModel.networkState.observe(viewLifecycleOwner, Observer {
             if (it) {
-                networkMessage.visibility = View.INVISIBLE
-                tvTryAgainConnect.visibility = View.INVISIBLE
+                binding.networkMessage.visibility = View.INVISIBLE
+                binding.tvTryAgainConnect.visibility = View.INVISIBLE
                 viewModel.setAdapter()
                 setPassenger()
             } else {
-                networkMessage.visibility = View.VISIBLE
-                tvTryAgainConnect.visibility = View.VISIBLE
+                binding.networkMessage.visibility = View.VISIBLE
+                binding.tvTryAgainConnect.visibility = View.VISIBLE
 
             }
         })
@@ -87,8 +83,8 @@ class PassengerFragment : Fragment() {
         } catch (e: Exception) {
             Toast.makeText(context, "Bir hata oluştu.", Toast.LENGTH_LONG).show()
             Log.e("Error", e.message.toString())
-            networkMessage.visibility = View.VISIBLE
-            tvTryAgainConnect.visibility = View.VISIBLE
+            binding.networkMessage.visibility = View.VISIBLE
+            binding.tvTryAgainConnect.visibility = View.VISIBLE
         }
 
     }
